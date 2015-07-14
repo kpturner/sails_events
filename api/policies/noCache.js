adac3c9924e58d3e54ae83cc5e08178393851960
Kevin Turner <kevin.turner@coraltree.co.uk>
2015-07-14 10:06:16 +0100

    Hackes for good old mobileIE10

--- api/policies/noCache.js
+++ api/policies/noCache.js
@@ -1,6 +1,6 @@
 module.exports = function (req, res, next) {
-    sails.log.info("Applying disable cache policy");
-	console.log("Applying disable cache policy");
+    //sails.log.info("Applying disable cache policy");
+	//console.log("Applying disable cache policy");
     res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
     res.header('Expires', '-1');
     res.header('Pragma', 'no-cache');  
---
+++ assets/js/dependencies/forMobileIE.js
@@ -0,0 +1,15 @@
+// This piece of JavaScript is to aid page responsiveness on IE running on a mobile device.
+// K Turner 14/07/15
+// See: http://getbootstrap.com/getting-started/#support-ie10-width
+
+// Copyright 2014-2015 Twitter, Inc.
+// Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+  var msViewportStyle = document.createElement('style')
+  msViewportStyle.appendChild(
+    document.createTextNode(
+      '@-ms-viewport{width:auto!important}'
+    )
+  )
+  document.querySelector('head').appendChild(msViewportStyle)
+}
