e6ae543a31cc24e3fec537f4cec5edb0c5c5a504
Kevin Turner <kevin.turner@coraltree.co.uk>
2015-09-15 10:00:45 +0100

    Remove mandatory lodge and lodge number when booking other people in.

--- assets/js/private/BookController.js
+++ assets/js/private/BookController.js
@@ -126,8 +126,8 @@ angular.module('EventsModule').controller('BookController', ['$scope', '$http', 
 	$scope.detailsComplete = function() {
 		var complete=true;
 		if (   (!$scope.bookingForm.name || $scope.bookingForm.name.length==0)
-			|| (!$scope.bookingForm.lodge || $scope.bookingForm.lodge.length==0)
-			|| (!$scope.bookingForm.lodgeNo || isNaN($scope.bookingForm.lodgeNo))
+			|| (!$scope.eventBookings && !$scope.userBookings && (!$scope.bookingForm.lodge || $scope.bookingForm.lodge.length==0))
+			|| (!$scope.eventBookings && !$scope.userBookings && (!$scope.bookingForm.lodgeNo || isNaN($scope.bookingForm.lodgeNo)))
 			|| (!$scope.eventBookings && !$scope.userBookings && (!$scope.bookingForm.email || $scope.bookingForm.email.length==0))			
 		) {
 			complete=false;
