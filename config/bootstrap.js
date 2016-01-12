713412c0e7ef75b13c94ea97c2f01410f3589c7a
Kevin Turner <kevin.turner@coraltree.co.uk>
2016-01-12 14:10:03 +0000

    Parameterise log level in  production

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -11,8 +11,6 @@
 
 module.exports.bootstrap = function(cb) {
   
-  console.log("Starting on port "+sails.config.port)
-  
   // Load the passport strategies
   sails.services.passport.loadStrategies();
   
--- config/env/production.js
+++ config/env/production.js
@@ -67,9 +67,9 @@ module.exports = {
    * Set the log level in production environment to "silent"                 *
    ***************************************************************************/
 
-  // log: {
-  //   level: "silent"
-  // }
+  log: {
+     level: process.env.LOGLEVEL || "silent"
+  }
   
   blueprints: {
     actions: false,
