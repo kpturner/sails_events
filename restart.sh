e4a744cd16664d34cb4d19c06e0cbfa9e81cbf99
Kevin Turner <kevin.turner@coraltree.co.uk>
2016-11-06 18:33:29 +0000

    restart.sh bug

--- restart.sh
+++ restart.sh
@@ -2,9 +2,10 @@
 service events-service stop
 service events-test stop
 service events-surrey stop
+
 service events-service start
 echo Event service started
-service events-test stop
+service events-test start
 echo Event test service started
-service events-surrey
+service events-surrey start
 echo Event Surrey service started
