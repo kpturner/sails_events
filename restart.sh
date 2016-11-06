fda4f0b9371147f8b2e43c4bcff84e66ff8442db
Kevin Turner <kevin.turner@coraltree.co.uk>
2016-11-06 18:28:31 +0000

    updateall.sh

--- restart.sh
+++ restart.sh
@@ -1,7 +1,10 @@
 # Restarting services
 service events-service stop
 service events-test stop
+service events-surrey stop
 service events-service start
 echo Event service started
 service events-test stop
 echo Event test service started
+service events-surrey
+echo Event Surrey service started
---
+++ views/updateall.sh
@@ -0,0 +1,6 @@
+cd /usr/sails/projects/events
+gitupdate.sh
+cd /usr/sails/projects/eventstest
+gitupdate.sh
+cd /usr/sails/projects/surrey
+gitupdate.sh
