6ece34b24706adb006a805716dc7cda2bf1ae1da
Kevin Turner <kevin.turner@coraltree.co.uk>
2017-04-19 15:45:53 +0100

    Don't need sudo

--- pgsl.sh
+++ pgsl.sh
@@ -1,1 +1,1 @@
-sudo SESS_ADAPTOR=memory SMTP_SENDER='PGSL Dining <dining@9263dining.org.uk>' NODE_ENV=production  node_modules/forever/bin/forever $1 app.js
+SESS_ADAPTOR=memory SMTP_SENDER='PGSL Dining <dining@9263dining.org.uk>' NODE_ENV=production  node_modules/forever/bin/forever $1 app.js
