bf6e25daf0f6652a3232c7942eb872b61e67149c
Kevin Turner <kevin.turner@coraltree.co.uk>
2017-04-19 15:57:50 +0100

    Added some instructions to pgsl.sh

--- pgsl.sh
+++ pgsl.sh
@@ -1,1 +1,5 @@
+# To start the application use 'bash  pgsl.sh start'
+# To stop the application use 'bash pgsl.sh stop' 
+
+
 SESS_ADAPTOR=memory SMTP_SENDER='PGSL Dining <dining@9263dining.org.uk>' NODE_ENV=production  node_modules/forever/bin/forever $1 app.js
