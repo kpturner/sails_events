a9ba27dfacedbc0e2782df970838c525730f32d7
Kevin Turner <k.turner.1@elsevier.com>
2018-04-11 18:02:12 +0100

    Cookie acceptance

--- assets/js/EventsModule.js
+++ assets/js/EventsModule.js
@@ -3,17 +3,17 @@
 // data as it was when the user navigated away
 window.onpageshow = function(event) {
 	if (event.persisted) {
-		window.location.reload() 
+		window.location.reload()
 	}
 };
 
 
 
-angular.module('EventsModule', ['ngDialog', 'ui.bootstrap', 'toastr', 'compareTo', 'akoenig.deckgrid'])
+angular.module('EventsModule', ['ngDialog', 'ui.bootstrap', 'toastr', 'compareTo', 'akoenig.deckgrid', 'angular-cookie-law'])
 		.directive('disableContents', function() {
 		    return {
 		        compile: function(tElem, tAttrs) {
-		            var inputs = tElem.find('input,select,textarea,button');					
+		            var inputs = tElem.find('input,select,textarea,button');
 		            inputs.not("[ng-disabled]").attr('ng-disabled', tAttrs['disableContents']);
 		            //for (var i = 0; i < inputs.length; i++) {
 		            //}
@@ -23,7 +23,7 @@ angular.module('EventsModule', ['ngDialog', 'ui.bootstrap', 'toastr', 'compareTo
 		.directive('populateNames',function(){
 			return function(scope, element, attrs) {
 		        element.change(function() {
-					// Get the directive value and convert to array - then find the targest and 
+					// Get the directive value and convert to array - then find the targest and
 					// change them if they are blank
 					var splits=element.val().split(" ");
 					try {
@@ -32,16 +32,16 @@ angular.module('EventsModule', ['ngDialog', 'ui.bootstrap', 'toastr', 'compareTo
 							var s=this.toString().split(":");
 							var i
 							if (!isNaN(parseInt(s[1]))) {
-								i=s[1];								
+								i=s[1];
 							}
 							else {
 								if (s[1].toLowerCase()=="last") {
-									i=splits.length-1;	
+									i=splits.length-1;
 								}
 								else if (s[1].toLowerCase()=="first") {
 									i=0;
-								}	 
-							}		
+								}
+							}
 							var $this=$("[name="+s[0]+']');
 							if ($this.length>0) {
 								if ($this.val().length==0) {
@@ -52,19 +52,19 @@ angular.module('EventsModule', ['ngDialog', 'ui.bootstrap', 'toastr', 'compareTo
 									$.each(ng,function(x,n){
 										if (x==ng.length-1) {
 											scopeObj[this.toString()]=splits[i];
-											$this.val(splits[i]);  
+											$this.val(splits[i]);
 											return false;
 										}
 										else
 											scopeObj=scopeObj[this.toString()]
-									})									
+									})
 								}
 							}
 						})
 					}
 					catch(e) {
 						console.log("Unable to action populateNames directive: "+e)
-					}		           
+					}
 		        });
 		    };
 		})
@@ -85,9 +85,9 @@ angular.module('EventsModule', ['ngDialog', 'ui.bootstrap', 'toastr', 'compareTo
 			 * be available in context:
 			 * 		scope
 			 * 		dataProperty
-			 * 		urn 
+			 * 		urn
 			 * 		queryString
-			 * 
+			 *
 			 */
 			function scroll(){
 				var $scope=this.scope;
@@ -96,49 +96,49 @@ angular.module('EventsModule', ['ngDialog', 'ui.bootstrap', 'toastr', 'compareTo
 					var urn=this.urn;
 					var queryString=this.queryString;
 					var augmentationFunction=this.augmentationFunction;
-				
-					// Use >= (not ==) if we want iPads to work 
+
+					// Use >= (not ==) if we want iPads to work
 					if($(window).scrollTop() + $(window).height() >= $(document).height()) {
 						// Hit bottom
 						$scope.scrollDisabled=true;
 						$scope.scrollPage+=1;
 						$scope.filterForm.criteria.page=$scope.scrollPage;
-						$scope.filterForm.criteria.limit=$scope.initialLimit;						
-						filter($scope,dataProperty,urn,queryString,augmentationFunction,false,true,function(sailsResponse){	
+						$scope.filterForm.criteria.limit=$scope.initialLimit;
+						filter($scope,dataProperty,urn,queryString,augmentationFunction,false,true,function(sailsResponse){
 							var data=sailsResponse.data[dataProperty]?sailsResponse.data[dataProperty]:sailsResponse.data;
 							if (data.length==0) {
 								// Out of data
 								$scope.scrollPage-=1;
-							}												
+							}
 							// Change the filter criteria to match what we really have
 							$scope.filterForm.criteria.limit=$scope.scrollPage*$scope.initialLimit;
 							$scope.filterForm.criteria.page=$scope.initialPage;
 							// Dummy get just to update the criteria server side
 							var uri=urn+encodeURIComponent(JSON.stringify($scope.filterForm.criteria))+"?nodata=1"+(queryString?("&"+queryString):"");
-							$http.get(uri); 	
+							$http.get(uri);
 							if (data.length>0) {
 								setTimeout(function(){
-									$scope.scrollDisabled=false;							
-								},500)	
-							}							
-						});			
-					}				
-				}			
+									$scope.scrollDisabled=false;
+								},500)
+							}
+						});
+					}
+				}
 			}
 
 			/**
 			 * filter
-			 * 
+			 *
 			 */
 			function filter($scope, dataProperty, urn, queryString, augmentationFunction, paging,scrolling,cb){
 				if (paging) {
 					$scope.filterForm.paging=true;
-					$scope.initialLimit=$scope.filterForm.criteria.limit;			
+					$scope.initialLimit=$scope.filterForm.criteria.limit;
 				}
 				else {
 					$scope.filterForm.loading=true;
-				}	
-				$scope.loading=true;			
+				}
+				$scope.loading=true;
 				// Submit request to server.
 				var uri=urn+encodeURIComponent(JSON.stringify($scope.filterForm.criteria))+(queryString?("?"+queryString):"")
 				$http.get(uri)
@@ -148,11 +148,11 @@ angular.module('EventsModule', ['ngDialog', 'ui.bootstrap', 'toastr', 'compareTo
 							// and use it of so - else assume it is just called "data"
 							var data=sailsResponse.data[dataProperty]?sailsResponse.data[dataProperty]:sailsResponse.data;
 							if (augmentationFunction) {
-								$scope[augmentationFunction](data);	
-							}							
+								$scope[augmentationFunction](data);
+							}
 							if (scrolling) {
 								// Add data to existing scope
-								$scope[dataProperty] = $.merge($scope[dataProperty],data);								
+								$scope[dataProperty] = $.merge($scope[dataProperty],data);
 							}
 							else {
 								$scope[dataProperty] = data;
@@ -160,28 +160,27 @@ angular.module('EventsModule', ['ngDialog', 'ui.bootstrap', 'toastr', 'compareTo
 							// Run callback if required
 							if (cb && typeof cb=="function") {
 								cb(sailsResponse);
-							}	
-							else {								
+							}
+							else {
 								$scope.scrollDisabled=false;
-							}		
+							}
 						}
 						else {
 							window.location="/";
 						}
 					})
 					.catch(function onError(sailsResponse){
-			
+
 						// Handle known error type(s).
 						toastr.error(sailsResponse.data, 'Error');
-						
-			
+
+
 					})
 					.finally(function eitherWay(){
 						$scope.filterForm.loading = false;
-						$scope.filterForm.paging = false;	
-						$scope.loading=false;													
+						$scope.filterForm.paging = false;
+						$scope.loading=false;
 					})
 			}
 
-		}]);	
- 
+		}]);
