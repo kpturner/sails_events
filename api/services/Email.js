cbc79a709b946bbcb442fed39cff673f666fd480
Kevin Turner <k.turner.1@elsevier.com>
2019-02-11 09:46:03 +0000

    Check mail trabsporter

--- api/services/Email.js
+++ api/services/Email.js
@@ -15,23 +15,27 @@
 
 
 module.exports = {
-  
+
   send: function(template,data,opts,cb) {
-     
-     // Check to see if we a reusing DKIM.  If not, we can just use sails-hook-email 
-     // functionality unaugmented 
-     
+
+     // Check to see if we a reusing DKIM.  If not, we can just use sails-hook-email
+     // functionality unaugmented
+
      // Use my localised email hook while waiting for a fix to sails-hook-email
      var hook="eventemail";
-     //var hook="email"; 
-     var transporter=sails.hooks[hook].transporter(); 
+     //var hook="email";
+     var transporter=sails.hooks[hook].transporter();
+     if (!transporter) {
+        sails.log.error('Unable to create mail transporter');
+        return;
+     }
      if (sails.config.events.DKIM && sails.config.events.DKIM.domainName && !transporter._dkim) {
         var nodemailer = require('nodemailer');
         var nodemailerDKIM = require('nodemailer-dkim');
         var fs = require('fs');
-        
+
         // Verify keys before attempting to use them.
-        // This checks that the configuration is correct and the private key matches the public key listed in DNS        
+        // This checks that the configuration is correct and the private key matches the public key listed in DNS
         var dkimOpts={
             domainName: sails.config.events.DKIM.domainName,
             keySelector: sails.config.events.DKIM.keySelector,
@@ -46,19 +50,19 @@ module.exports = {
                         sails.log.verbose('DKIM verification successful');
                         transporter._dkim=true; // Stops us initialising it again
                         // Tell it to stream correctly with the DKIM header(s)
-                        transporter.use('stream', nodemailerDKIM.signer(dkimOpts));                         
+                        transporter.use('stream', nodemailerDKIM.signer(dkimOpts));
                 }
                 // Send the email
-                sails.hooks[hook].send(template,data,opts,cb);     	
-        }); 
-     } 
+                sails.hooks[hook].send(template,data,opts,cb);
+        });
+     }
      else {
         // Send the email
-        sails.hooks[hook].send(template,data,opts,cb);     	        
+        sails.hooks[hook].send(template,data,opts,cb);
      }
-     	
-     
+
+
   },
 
-   
+
 };
