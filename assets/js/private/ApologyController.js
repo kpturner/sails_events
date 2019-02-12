314fccb0378be64db4136732f71ce0380f6f1b45
Kevin Turner <k.turner.1@elsevier.com>
2019-02-12 10:30:51 +0000

    Fix apology

--- assets/js/private/ApologyController.js
+++ assets/js/private/ApologyController.js
@@ -1,37 +1,36 @@
 angular.module('EventsModule').controller('ApologyController', ['$scope', '$http', 'toastr', 'ngDialog', function($scope, $http, toastr, ngDialog){
 
-	
+
 	$scope.apologyForm = {
 		loading: false
 	}
 
-		
-	// Initialise "user" in the scope with the data set in the view script 
+
+	// Initialise "user" in the scope with the data set in the view script
 	$scope.user=SAILS_LOCALS.user;
 	$scope.event=SAILS_LOCALS.event;
-	$scope.mode=SAILS_LOCALS.mode;
-	$scope.existingBooking=SAILS_LOCALS.bookingId;
+	$scope.mode=SAILS_LOCALS.mode;;
 	$scope.existingApology=SAILS_LOCALS.apology.id;
 	if ($scope.existingApology)
-		$scope.apologyForm.message = SAILS_LOCALS.apology.message;	
-	
+		$scope.apologyForm.message = SAILS_LOCALS.apology.message;
+
 	/**
 	 * Submit apology
-	 */	
+	 */
 	$scope.submitApologyForm = function(){
-		
+
 		$scope.apologyForm.loading=true;
-		
+
 		$http.post('/sendapology?selecteduserid='+SAILS_LOCALS.selectedUserId, {
             _csrf: SAILS_LOCALS._csrf,
 			eventid: SAILS_LOCALS.event.id,
-			message: $scope.apologyForm.message			 
+			message: $scope.apologyForm.message
 		})
 		.then(function onSuccess(sailsResponse){
-			toastr.success("You have successfully sent apologies")	
+			toastr.success("You have successfully sent apologies")
 			setTimeout(function(){
-				window.location = '/'	
-			},1000)				
+				window.location = '/'
+			},1000)
 		})
 		.catch(function onError(sailsResponse){
 
@@ -43,7 +42,7 @@ angular.module('EventsModule').controller('ApologyController', ['$scope', '$http
 		.finally(function eitherWay(){
 			//$scope.bookingForm.loading = false;
 		})
-		
-	}	 
+
+	}
 
 }])
