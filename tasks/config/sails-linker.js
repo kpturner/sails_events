ae3e55f4f3cbdb9033fa286c3ff28823fec1e7e0
Kevin Turner <k.turner.1@elsevier.com>
2019-07-04 18:21:44 +0100

    Cachebust production

--- tasks/config/sails-linker.js
+++ tasks/config/sails-linker.js
@@ -47,7 +47,7 @@ module.exports = function(grunt) {
 			options: {
 				startTag: '<!--SCRIPTS-->',
 				endTag: '<!--SCRIPTS END-->',
-				fileTmpl: '<script src="%s"></script>',
+				fileTmpl: '<script src="%s?cachebust=' + new Date().getTime() + '"></script>',
 				appRoot: '.tmp/public'
 			},
 			files: {
@@ -107,7 +107,7 @@ module.exports = function(grunt) {
 			options: {
 				startTag: '<!--STYLES-->',
 				endTag: '<!--STYLES END-->',
-				fileTmpl: '<link rel="stylesheet" href="%s">',
+				fileTmpl: '<link rel="stylesheet" href="%s?cachebust=' + new Date().getTime() + '">',
 				appRoot: '.tmp/public'
 			},
 			files: {
