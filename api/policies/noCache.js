d61cc835aded15a17f4cadbf92474e6699c18392
Kevin <kevin@kpturner.co.uk>
2022-11-17 12:04:00 +0000

    Try to trap undefined _csrf

--- api/policies/noCache.js
+++ api/policies/noCache.js
@@ -4,10 +4,5 @@ module.exports = function (req, res, next) {
     res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
     res.header('Expires', '-1');
     res.header('Pragma', 'no-cache');
-    try {
-      next();
-    } catch (err) {
-      sails.log.debug(err);
-      throw 'Unexpected error occurred';
-    }
+    next();
 };
