3c9e61ff3a75f17a14e4161310d4278116a240d8
Kevin <kevin@kpturner.co.uk>
2024-05-22 19:07:35 +0100

    Increase menu choices to 10

--- sql/archive/update202300.sql
+++ sql/archive/update202300.sql
@@ -1,3 +1,10 @@
+ALTER TABLE `linkedbooking`
+  ADD COLUMN `carReg` varchar(255) DEFAULT NULL;
+ALTER TABLE `event`
+  ADD COLUMN `regReqd` tinyint(1) DEFAULT NULL;
+ALTER TABLE `booking`
+  ADD COLUMN `carReg` varchar(255) DEFAULT NULL;
+
 ALTER TABLE `event`
   ADD COLUMN `menu5` longtext AFTER `menu4`
     ADD COLUMN `menu6` longtext AFTER `menu5`
