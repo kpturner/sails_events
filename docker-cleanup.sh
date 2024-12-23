e0dbf4ac095442008ef38f1e9f87c4296bffa6c1
Kevin <kevin@kpturner.co.uk>
2024-12-23 15:37:57 +0000

    debug service

--- docker-cleanup.sh
+++ docker-cleanup.sh
@@ -1,4 +1,6 @@
-sudo docker system prune -a -f
-sudo docker rm -v $(sudo docker ps -a -q -f status=exited)
-sudo docker rmi -f  $(sudo docker images -f "dangling=true" -q)
-docker volume ls -qf dangling=true | xargs -r docker volume rm
+# sudo docker system prune -a -f
+# sudo docker rm -v $(sudo docker ps -a -q -f status=exited)
+# sudo docker rmi -f  $(sudo docker images -f "dangling=true" -q)
+# docker volume ls -qf dangling=true | xargs -r docker volume rm
+
+echo "HELLO KEVIN"
